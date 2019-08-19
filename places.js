ae135a5794837b83af99d7a09febac676df479b7
dpamar <dpamar@gmail.com>
2019-08-19 22:03:38 +0200

    Update places.js

--- places.js
+++ places.js
@@ -1,3 +1,17 @@
 var places = [
+    [45.180833, 5.722511, 'Juste en face voyons !'],
+    [45.181607, 5.722721, 'En allant vers la caserne de Bonne'],
+    [45.1824, 5.724172, 'On ne peut plus y voir Monsieur Bout de bois'],
+    [45.183359, 5.723691, 'La maison de Grenoble de Papi et Mamie'],
+    [45.183823, 5.725057, 'Les jets d eau sont fermés '],
+    [45.184054, 5.724284, 'Une baguette s il vous plait '],
+    [45.184364, 5.723761, 'Vive mon école !!!'],
+    [45.185045, 5.723381, 'Papa il dit la Pistouche'],
+    [45.186641, 5.723727, 'Non on n achète pas de jeu'],
+    [45.187727, 5.723573, 'Un autre jet d eau mais qui marche'],
+    [45.188101, 5.724478, 'Un magasin de musique '],
+    [45.188876, 5.725003, 'Encore plus haut le jet d eau'],
+    [45.190181, 5.726228, 'En suivant les rails du tram'],
+    [45.190986, 5.727627, 'Ouais le manège !!!']
 ];
-
+//Resume game designer URL: https://dpamar.github.io/tresor20aout/generator.html?places=W1s0NS4xODA4MzMsIDUuNzIyNTExLCAnSnVzdGUgZW4gZmFjZSB2b3lvbnMgISddLFs0NS4xODE2MDcsIDUuNzIyNzIxLCAnRW4gYWxsYW50IHZlcnMgbGEgY2FzZXJuZSBkZSBCb25uZSddLFs0NS4xODI0MDAsIDUuNzI0MTcyLCAnT24gbmUgcGV1dCBwbHVzIHkgdm9pciBNb25zaWV1ciBCb3V0IGRlIGJvaXMnXSxbNDUuMTgzMzU5LCA1LjcyMzY5MSwgJ0xhIG1haXNvbiBkZSBHcmVub2JsZSBkZSBQYXBpIGV0IE1hbWllJ10sWzQ1LjE4MzgyMywgNS43MjUwNTcsICdMZXMgamV0cyBkIGVhdSBzb250IGZlcm3pcyAnXSxbNDUuMTg0MDU0LCA1LjcyNDI4NCwgJ1VuZSBiYWd1ZXR0ZSBzIGlsIHZvdXMgcGxhaXQgJ10sWzQ1LjE4NDM2NCwgNS43MjM3NjEsICdWaXZlIG1vbiDpY29sZSAhISEnXSxbNDUuMTg1MDQ1LCA1LjcyMzM4MSwgJ1BhcGEgaWwgZGl0IGxhIFBpc3RvdWNoZSddLFs0NS4xODY2NDEsIDUuNzIzNzI3LCAnTm9uIG9uIG4gYWNo6HRlIHBhcyBkZSBqZXUnXSxbNDUuMTg3NzI3LCA1LjcyMzU3MywgJ1VuIGF1dHJlIGpldCBkIGVhdSBtYWlzIHF1aSBtYXJjaGUnXSxbNDUuMTg4MTAxLCA1LjcyNDQ3OCwgJ1VuIG1hZ2FzaW4gZGUgbXVzaXF1ZSAnXSxbNDUuMTg4ODc2LCA1LjcyNTAwMywgJ0VuY29yZSBwbHVzIGhhdXQgbGUgamV0IGQgZWF1J10sWzQ1LjE5MDE4MSwgNS43MjYyMjgsICdFbiBzdWl2YW50IGxlcyByYWlscyBkdSB0cmFtJ10sWzQ1LjE5MDk4NiwgNS43Mjc2MjcsICdPdWFpcyBsZSBtYW7oZ2UgISEhJ11d
